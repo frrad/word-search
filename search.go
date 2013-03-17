0e6522d5ae3eb286562673bc1953172286bf7df9
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-17 00:10:21 -0700

    First hacky solution

--- search.go
+++ search.go
@@ -6,14 +6,17 @@ import (
 )
 
 func readFile(filename string, numRows int, numColumns int) (answer [][]byte) {
+
 	filecontents, err := ioutil.ReadFile(filename)
+
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(filecontents)
-
+	//initialize a 2-d slice 
 	answer = make([][]byte, numRows)
+
+	//read values in from file.
 	for row := 0; row < numRows; row++ {
 		answer[row] = make([]byte, numColumns)
 
@@ -22,13 +25,72 @@ func readFile(filename string, numRows int, numColumns int) (answer [][]byte) {
 		}
 	}
 
-	fmt.Println(answer)
+	//Print grid for debugging purposes
+	/*
+		for _, row := range answer {
+			for _, char := range row {
+				fmt.Print(char)
+			}
+			fmt.Println("")
+
+		}
+	*/
 
 	return
 }
 
-func main() {
-	fmt.Println("hi! world")
+func match(grid [][]byte, word []byte, xstart int, ystart int,
+	xdir int, ydir int) bool {
 
-	readFile("test.txt", 3, 7)
+	wordlength := len(word)
+
+	xsize := len(grid)
+	ysize := len(grid[0])
+
+	//Check if we fall off grid
+	if (wordlength-1)*xdir+xstart > xsize || (wordlength-1)*ydir+ystart > ysize ||
+		(wordlength-1)*xdir+xstart < 0 || (wordlength-1)*ydir+ystart < 0 {
+		return false
+	}
+
+	for i := 0; i < wordlength; i++ {
+		if word[i] != grid[xstart+(i*xdir)][ystart+(i*ydir)] {
+			return false
+		}
+	}
+	return true
+
+}
+
+func wordByte(word string) (answer []byte) {
+
+	answer = make([]byte, len(word))
+
+	for i, letter := range word {
+		answer[i] = byte(letter)
+	}
+	return
+}
+
+func main() {
+
+	numrows := 3
+	numcols := 7
+	grid := readFile("test.txt", numrows, numcols)
+	word := wordByte("TME")
+
+	for i := 0; i < numrows; i++ {
+		for j := 0; j < numcols; j++ {
+
+			for a := -1; a < 2; a++ {
+				for b := -1; b < 2; b++ {
+					if match(grid, word, i, j, a, b) {
+						fmt.Println("(", 1+i, ",", j+1, ")", a, b)
+					}
+				}
+			}
+
+		}
+	}
+
 }
